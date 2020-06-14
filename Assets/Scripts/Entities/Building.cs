e6fd8796299658bda09c3887015c2eabfd6de28e
Jakob Ambsdorf <jakob.ambsdorf@gmail.com>
2020-06-14 22:20:38 +0200

    Refactor Building to ProductionBuilding and abstract base Building class

--- Assets/Scripts/Entities/Building.cs
+++ Assets/Scripts/Entities/Building.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Tile;
+using static GameManager;
+
 
 public abstract class Building : MonoBehaviour
 {
-    #region Manager References
-    JobManager _jobManager; //Reference to the JobManager
+    public enum BuildingTypes { Empty, Fishery, Lumberjack, Sawmill, SheepFarm, Knitters, PotatoFarm, Distillery };
+
+    #region Attributes
+    public BuildingTypes _type; //The type of the building
+    public int _upkeep; //The money cost per minute
+    public int _buildCostMoney; //placement money cost
+    public int _buildCostPlanks; //placement planks cost
+    public Tile _tile; //Reference to the tile it is built on
+    public List<TileTypes> _canBeBuiltOnTileTypes; //A restriction on which types of tiles it can be placed on
     #endregion
-    
+
+    #region Manager References
+    protected JobManager _jobManager; //Reference to the JobManager
+    protected GameManager _GM; // Reference to the GameManager
+    #endregion
+
     #region Workers
     public List<Worker> _workers; //List of all workers associated with this building, either for work or living
     #endregion
@@ -15,9 +30,21 @@ public abstract class Building : MonoBehaviour
     #region Jobs
     public List<Job> _jobs; // List of all available Jobs. Is populated in Start()
     #endregion
-    
 
     #region Methods   
+
+    // <summary> Initilize needs to be called on any instantiated tile before it is used. </summary>
+    public virtual void Inintilize(Tile parentTile)
+    {
+        _tile = parentTile;
+    }
+
+    public bool CanBeBuiltOnTile(Tile T)
+    {
+        return _canBeBuiltOnTileTypes.Contains(T._type);
+    }
+
+
     public void WorkerAssignedToBuilding(Worker w)
     {
         _workers.Add(w);
@@ -28,4 +55,9 @@ public abstract class Building : MonoBehaviour
         _workers.Remove(w);
     }
     #endregion
+
+    protected virtual void Start()
+    {
+        _GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 }
--- Assets/Scripts/Entities/ProductionBuilding.cs
+++ Assets/Scripts/Entities/ProductionBuilding.cs
@@ -4,55 +4,34 @@ using UnityEngine;
 using static Tile;
 using static GameManager;
 
-public class ProductionBuilding : MonoBehaviour {
-
-    public enum BuildingTypes { Empty, Fishery, Lumberjack, Sawmill, SheepFarm, Knitters, PotatoFarm, Distillery };
-
-    private GameManager _GM;
-
-    #region Attributes
-    public BuildingTypes _type; //The type of the building
-    public int _upkeep; //The money cost per minute
-    public int _buildCostMoney; //placement money cost
-    public int _buildCostPlanks; //placement planks cost
-    public Tile _tile; //Reference to the tile it is built on
-
+public class ProductionBuilding : Building {
+    
+    #region ProductionAttributes
     public float efficiencyValue; //Calculated based on the surrounding tile types
     public float resourceGenerationInterval; //If operating at 100% efficiency, this is the time in seconds it takes for one production cycle to finish
     public int _outputCount; //The number of output resources per generation cycle (for example the Sawmill produces 2 planks at a time)
 
-    public List<TileTypes> _canBeBuiltOnTileTypes; //A restriction on which types of tiles it can be placed on
     public TileTypes _efficiencyScalesWithNeighboringTiles; //A choice if its efficiency scales with a specific type of surrounding tile
     public int[] _minMaxNeighbors; //The minimum and maximum number of surrounding tiles its efficiency scales with (0-6)
 
     public List<ResourceTypes> _inputResources; //A choice for input resource types (0, 1 or 2 types)
     public ResourceTypes _outputResource; //A choice for output resource type
-
     #endregion
 
-    public bool CanBeBuiltOnTile(Tile T){
-        return _canBeBuiltOnTileTypes.Contains(T._type);
+    public override void Inintilize(Tile parentTile)
+    {
+        base.Inintilize(parentTile);
+        calculateEfficiency();
     }
 
     private float timer;
     private float resTimer;
-
-    void Awake(){
-        _GM = GameObject.Find("GameManager").GetComponent<GameManager>();
-    }
-
-    public void inintilize(Tile parentTile)
-    {
-        _tile = parentTile;
-        this.calculateEfficiency();
-    }
-
     private void calculateEfficiency()
     {
         if (_minMaxNeighbors.Length == 0)
         {
             // no efficiency scaling -> always 1
-            this.efficiencyValue = 1;
+            efficiencyValue = 1;
             return;
         }
 
@@ -68,17 +47,22 @@ public class ProductionBuilding : MonoBehaviour {
 
         if (posInRange <= 0)
         {
-            this.efficiencyValue = 0;
+            efficiencyValue = 0;
         }
         else
         {
-            this.efficiencyValue = (float) posInRange / range;
+            efficiencyValue = (float) posInRange / range;
         }
     }
 
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+
     void Update(){
         timer = Time.deltaTime;
-
         resTimer += timer * efficiencyValue;
 
         if (resTimer >= resourceGenerationInterval){
