7e220df3156e3e604a49a4fc4bd897914afe20f1
Jakob Ambsdorf <jakob.ambsdorf@gmail.com>
2020-06-17 13:54:29 +0200

    Add Housing Building

---
+++ Assets/Scripts/Entities/HousingBuilding.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class HousingBuilding : Building
+{
+    public int _spawnInterval;
+    public int _workerCapacity;
+    public int _initialWorkerCount;
+    public GameObject _workerPrefab;
+
+    private float _spawnTimer;
+    public void Update()
+    {
+        _spawnTimer += Time.deltaTime;
+
+        if (_spawnTimer >= _spawnInterval)
+        {
+            if (_workers.Count < _workerCapacity)
+
+            {
+                spawnWorker();
+            }
+            _spawnTimer = 0;
+        }
+    }
+
+    private void spawnWorker()
+    {
+        GameObject instance = Instantiate(_workerPrefab, transform.position, transform.rotation);
+        // set parent tile
+        Worker newWorker = instance.GetComponent<Worker>();
+        _workers.Add(newWorker);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        // spawn initial workers
+        for (int i = 0; i < _initialWorkerCount; i++)
+        {
+            spawnWorker();
+        }
+    }
+    public override void Inintilize(Tile parentTile)
+    {
+        base.Inintilize(parentTile);
+    }
+}
